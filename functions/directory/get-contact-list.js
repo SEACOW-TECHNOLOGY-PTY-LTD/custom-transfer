4f5455e9761cc15d2e77ad46baf755845d65a949
RAN <cs.pangge@gmail.com>
2021-10-18 01:25:16 +1100

    fix bugs

--- functions/directory/get-contact-list.js
+++ functions/directory/get-contact-list.js
@@ -5,21 +5,36 @@ let utils = require(path)
 
 exports.handler = TokenValidator(async (context, event, callback) => {
   const contacts = [
-    {
-      sid: 'directory1',
-      fullName: 'Ran Tao',
-      phone: '+61280068818',
-    },
+    { sid: 'directory1', fullName: 'Anna Zawada', phone: '+61287902722' },
     {
       sid: 'directory2',
-      fullName: 'Anant Chitale',
-      phone: '+61433102802',
+      fullName: 'Anna Zawada - Mobile',
+      phone: '+61448488321',
     },
     {
       sid: 'directory3',
-      fullName: 'Anna Zawada',
-      phone: '+61448488321',
-    }
+      fullName: 'Sam - Repair Centre',
+      phone: '+61287902727',
+    },
+    { sid: 'directory4', fullName: 'Sam - RC Mobile', phone: '+61425764905' },
+    { sid: 'directory5', fullName: 'Suzana Goceva', phone: '+61287902726' },
+    { sid: 'directory6', fullName: 'Sandra He', phone: '+61287902725' },
+    {
+      sid: 'directory7',
+      fullName: 'Sandra He - Mobile',
+      phone: '+61448421255',
+    },
+    { sid: 'directory8', fullName: 'James - Warehouse', phone: '+61448328855' },
+    {
+      sid: 'directory9',
+      fullName: 'Nance Mason - Sales',
+      phone: '+61457529918',
+    },
+    {
+      sid: 'directory10',
+      fullName: 'Warren Merritt - Mobile',
+      phone: '+61419741140',
+    },
   ]
 
   callback(null, utils.response('json', contacts))
